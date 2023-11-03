8a45ff777459403f9534009682336466a4faed6c
Angel Cervantes <cs.angelcervantes@gmail.com>
2023-11-03 18:06:37 -0500

    fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
 
 const GameGrid = () => {
-  const [game, setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+
+  interface Game {
+    id: number;
+    name: string;
+  }
+
+  interface FetchGameResponse {
+    count: number;
+    results: Game[];
+  }
 
   useEffect(() => {
     apiClient
@@ -22,12 +23,11 @@ const GameGrid = () => {
       .then((res) => setGames(res.data.results))
       .catch((err) => setError(err.message));
   });
-
   return (
     <>
       {error && <Text>{error}</Text>}
       <ul>
-        {game.map((game) => (
+        {games.map((game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
